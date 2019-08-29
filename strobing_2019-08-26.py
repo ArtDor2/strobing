5eb6e4af87893d5a03fdff2c807f94985d501bff
Artur Dorovskikh <19955193+ArtDor2@users.noreply.github.com>
2019-08-28 21:57:52 -0500

    fixed the mistake of reverse strobing
    Now actually strobing instead of displaying black frame once every 6 frames the screen is displayed every 6 frames. The strobing is fully functional now but has frequent stuttering, to fix I need to add custom timer, not "clock.tick(FPS) from pygame.
    
    The "if game_frames < 6" is what determines how much blur is reduced, 2 for half reduction in framerate and blur, 6 for 144hz will show display 24fps (for movies with 24fps) while showing black the other 120fps.

--- strobing_2019-08-26.py
+++ strobing_2019-08-26.py
@@ -10,38 +10,51 @@ import win32gui
 
 pygame.init()
 # pygame.mixer.quit()
-screen = pygame.display.set_mode((720, 540), pygame.NOFRAME) # For borderless, use pygame.NOFRAME
+screen_width = 720
+screen_height = 540
+
+screen = pygame.display.set_mode((screen_width, screen_height), pygame.NOFRAME) # For borderless, use pygame.NOFRAME
 done = False
 fuchsia = (255, 0, 128)  # Transparency color
 # dark_red = (139, 0, 0)
 game_frames = 0
 
-# Set window transparency color
+# Set window transparency color to hide window border
 hwnd = pygame.display.get_wm_info()["window"]
 win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE,
                        win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED)
 win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(*fuchsia), 0, win32con.LWA_COLORKEY)
 
-obj_transparancy = 1
-FPS = 120
+
 clock = pygame.time.Clock() # Create a clock object
+FPS = 144 # set to your screen refresh rate
+
+#? possible optimization to stuttering?
+#myimage = pygame.image.load("black square.png")
+#myimage = pygame.transform.scale(myimage, (screen_width, screen_height))
+#imagerect = myimage.get_rect()
+
+#screen.set_alpha(256/2) # the higher the alpha the more blur reduction
+game_frames = 1
 
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
 
-    if obj_transparancy == 1:
-        # pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(100, 100, 600, 600))
-        screen.fill(fuchsia)
-    else:
-        screen.fill((0,0,0))  # Transparent background
-    
-    pygame.display.flip()
-    # pygame.display.update()
-    clock.tick(FPS)
+    if game_frames < 6: #2 minimum, cuts frames displayed in half, 6 from 144hz for 24fps
+        #screen.blit(myimage, imagerect)
+        #self.image.fill((255, 255, 255, alpha), None, pygame.BLEND_RGBA_MULT)
 
-    game_frames += 1
-    if game_frames == 4:
-        game_frames = 0
-        obj_transparancy *= -1
+        screen.fill((0,0,0))  # black
+        game_frames += 1
+    else:
+        #screen.blit(myimage, imagerect)
+
+        screen.fill(fuchsia)
+        game_frames = 1
+
+    #pygame.display.flip() #update the entire surface
+    pygame.display.update() #update part of the surface
+
+    clock.tick(FPS)
